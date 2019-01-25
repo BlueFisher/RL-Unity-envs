49efb00f5ddd35d2e7a7a268a68826ba778ab9bd
Fisher Chang <blue_fisher@qq.com>
2019-01-25 10:03:51 +0800

    add walker

--- Assets/Walker/WalkerAcademy.cs
+++ Assets/Walker/WalkerAcademy.cs
@@ -1,16 +1,27 @@
-﻿using MLAgents;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using MLAgents;
 
-namespace SimpleRoller {
-    public class RollerAcademy : Academy {
+namespace Walker {
+    public class WalkerAcademy : Academy {
         public GameObject AreaPrefab;
         private List<GameObject> Areas;
         private int LastCopy = 1;
+
         public override void InitializeAcademy() {
+            Monitor.verticalOffset = 1f;
+
+            // We increase the Physics solver iterations in order to
+            // make walker joint calculations more accurate.
+            Physics.defaultSolverIterations = 12;
+            Physics.defaultSolverVelocityIterations = 12;
+            Time.fixedDeltaTime = 0.01333f; //(75fps). default is .2 (60fps)
+            Time.maximumDeltaTime = .15f; // Default is .33
+
             Areas = new List<GameObject>();
         }
+        
         public override void AcademyReset() {
             int copy = (int)resetParameters["copy"];
             if (copy > 1 && copy != LastCopy) {
@@ -20,7 +31,7 @@ namespace SimpleRoller {
                 }
                 Areas.Clear();
                 for (int i = 0; i < copy - 1; i++) {
-                    GameObject area = Instantiate(AreaPrefab, new Vector3((i + 1) * 30, 0, 0), Quaternion.identity) as GameObject;
+                    GameObject area = Instantiate(AreaPrefab, new Vector3((i + 1) * 11, 0, 0), Quaternion.identity) as GameObject;
                     Areas.Add(area);
                 }
             }
